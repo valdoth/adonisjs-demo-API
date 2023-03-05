180112457258c77b964febe854138c226920f519
valdoth <valdotsiarohasina@gmail.com>
2023-03-06 01:14:58 +0300

    project done

--- start/routes.ts
+++ start/routes.ts
@@ -20,7 +20,14 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return "Hello word"
-})
-   
+Route.get('/', "HomeController.index")
+
+Route.group(() => {
+    Route.group(() => {
+        Route.get('/todo', 'TodosController.index')
+        Route.post('/todo', 'TodosController.store')
+        Route.patch('/todo/:id', 'TodosController.update')
+    }).middleware('auth')
+    Route.post('/register', 'AuthController.index')
+    Route.post('/login', 'AuthController.login')
+}).prefix('api')
